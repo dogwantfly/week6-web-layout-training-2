b8a5a4a66f221bbacf1267e7a104631bb3bbada5
hsin-yu <tina2793778@gmail.com>
2020-11-22 22:49:24 +0800

    Update 2020-11-22T14:49:05.794Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-  //選單
-  $('.js-ham').click( function (e) {
-    e.preventDefault();
-    $('.js-menu').slideToggle(500);
-  });
- 
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//選單
+$('.js-ham').click(function (e) {
+  e.preventDefault();
+  $('.js-menu').slideToggle(500);
+});
+//# sourceMappingURL=all.js.map
